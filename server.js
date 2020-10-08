e5edc092041cad693578a2883682d48f42148625
A <jesus.e.estrada@hotmail.com>
2020-10-08 16:04:12 -0500

    server running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Welcome');
+})
+
+module.exports = server;
